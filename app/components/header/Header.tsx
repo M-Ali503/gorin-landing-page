fd1fc15a4a6993317151922d7481cb4fdbd29630
ali <muhammadali503.se@gmail.com>
2024-11-19 17:05:32 +0500

    Add header

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,38 @@
+// import React from 'react'
+import Image from "next/image";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div>
+      <Image
+          className="dark:invert"
+          src="/sellcre_logo.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38} 
+          priority
+        />
+      </div>
+      <nav className={styles.navigation}>
+        <ul>
+          <li>
+            <a href="#">feature</a>
+          </li>
+          <li>
+            <a href="#">pricing</a>
+          </li>
+          <li>
+            <a href="#">login</a>
+          </li>
+          <li>
+            <button className={styles.tryButton}>try for free</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
